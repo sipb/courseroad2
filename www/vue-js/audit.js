b41003710a5982299c3a7286f3840334220879b5
Nate Foss <npfoss@gmail.com>
2018-10-13 14:48:31 -0400

    load course requirements!

--- www/vue-js/audit.js
+++ www/vue-js/audit.js
@@ -3,12 +3,27 @@
 // var sourceOfTruth = require("../source-of-truth");
 // import {sourceOfTruth} from "../source-of-truth";
 
+// Vue.component('req-tree', {
+//     props: ['req-name'],
+//     template: '<li>{{ todo.text }}</li>'
+// })
+
+
+
+// Vue.use(VueResource);
 const DegreeAudit = new Vue({
   el: '#degree-audit',
-  // data: sourceOfTruth,
-  computed: {
-    message() {
-        return sourceOfTruth.selectedSubjects[0].id;
-    }
+  data: {
+    message:'loading audit...'
+  },
+  mounted() {
+    // TODO: this is kind of janky, and should not happen ideally:
+    //  I'm bouncing the request through this proxy to avoid some issue with CORS
+    //  see this issue for more: https://github.com/axios/axios/issues/853
+    return axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://fireroad-dev.mit.edu/requirements/get_json/major6-3`)
+      .then(response => {
+        this.message = response.data;
+      })
+    // return sourceOfTruth.selectedSubjects[0].id;
   }
 })
