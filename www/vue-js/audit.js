1a1fcfab2328576945572dc69c7c4648f46e05e6
Nate Foss <npfoss@gmail.com>
2018-10-13 21:31:38 -0400

    format course requirements in a nice tree!

--- www/vue-js/audit.js
+++ www/vue-js/audit.js
@@ -3,27 +3,69 @@
 // var sourceOfTruth = require("../source-of-truth");
 // import {sourceOfTruth} from "../source-of-truth";
 
-// Vue.component('req-tree', {
-//     props: ['req-name'],
-//     template: '<li>{{ todo.text }}</li>'
-// })
+Vue.component('req-tree', {
+  props: ['root'],
+  data: function() {
+    return {
+      showChildren: false,
+    }
+  },
+  template:
+    `<li>
+      <div class="req-tree">
+        <div>{{ root.title }}</div>
+        <div style="font-style:italic">{{ root['threshold-desc'] }}</div>
+        <div>{{ root.req }}</div>
+        <a href="#" @click="toggleChildren" v-if="'reqs' in root"> show/hide </a>
+        <ul v-if="showChildren">
+          <req-tree 
+            v-for="node in root.reqs" 
+            :root="node" 
+          >
+          </req-tree>
+        </ul>
+      </div>
+    </li>`,
+    methods: {
+      toggleChildren() {
+        this.showChildren = !this.showChildren;
+      }
+    }
+})
 
-
-
-// Vue.use(VueResource);
 const DegreeAudit = new Vue({
   el: '#degree-audit',
   data: {
-    message:'loading audit...'
-  },
+    selectedReqs: ['major6-3','major18gm',],
+    reqTrees: {
+      'major6-3': { // unfortunately have to have this here or it crashes on key lookup in the beginning...
+          title: 'loading...',
+          reqs: []
+        },
+      'major18gm': {
+          title: 'loading...',
+          reqs: []
+        },
+      },
+    reqList: [],
+    selected: '',
+  },  
   mounted() {
     // TODO: this is kind of janky, and should not happen ideally:
     //  I'm bouncing the request through this proxy to avoid some issue with CORS
     //  see this issue for more: https://github.com/axios/axios/issues/853
-    return axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://fireroad-dev.mit.edu/requirements/get_json/major6-3`)
+    axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://fireroad-dev.mit.edu/requirements/list_reqs/`)
       .then(response => {
-        this.message = response.data;
+        this.reqList = response.data;
+      })
+    axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://fireroad-dev.mit.edu/requirements/get_json/major6-3`)
+      .then(response => {
+        this.reqTrees['major6-3'] = response.data;
+      })
+    axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://fireroad-dev.mit.edu/requirements/get_json/major18gm`)
+      .then(response => {
+        this.reqTrees['major18gm'] = response.data;
       })
     // return sourceOfTruth.selectedSubjects[0].id;
-  }
+  },
 })
