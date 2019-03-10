0157c33921772ff081a7c7661a2575bc9a19d35d
Nate Foss <npfoss@gmail.com>
2019-03-09 21:00:21 -0500

    indentation

--- src/components/SubjectScroll.vue
+++ src/components/SubjectScroll.vue
@@ -1,23 +1,27 @@
 <template>
   <v-container grid-list-md>
     <v-layout row overflow-x-scroll>
-        <v-flex v-for = "subject in subjects">
-          <v-card class = "subject" color = "primary" @click = "$emit('click-subject', subject.subject_id)">
-            <v-card-text>
-              <div><b>{{subject.subject_id}}</b></div>
-              {{subject.title}}
-            </v-card-text>
-          </v-card>
-        </v-flex>
+      <v-flex v-for = "subject in subjects">
+        <v-card class = "subject" color = "primary" @click = "$emit('click-subject', subject.subject_id)">
+          <v-card-text>
+            <div><b>{{subject.subject_id}}</b></div>
+            {{subject.title}}
+          </v-card-text>
+        </v-card>
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
+
+
 <script>
 export default {
   name: "SubjectScroll",
   props: ["subjects"]
 }
 </script>
+
+
 <style>
 .overflow-x-scroll {
   overflow-x: scroll;
