1fb70ea95db6480b3cce5653fc79dae6c2c72e03
Georgia Shay <georgiashay@gmail.com>
2019-03-24 23:38:00 -0400

    Removed validCourses and validGeneric from Class.vue

--- src/components/Class.vue
+++ src/components/Class.vue
@@ -28,15 +28,7 @@ export default {
   data() {
     return {
       isSatisfied: true,
-      dragSemesterNum: -1,
-      validCourses: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
-        '11', '12', '14', '15', '16', '17', '18', '20', '21', '21A', '21W',
-        'CMS', '21G', '21H', '21L', '21M', 'WGS', '22', '24', 'CC', 'CSB',
-        'EC', 'EM', 'ES', 'HST', 'IDS', 'MAS', 'SCM', 'STS', 'SWE', 'SP'
-      ],
-      validGeneric: ["PHY1", "PHY2", "CHEM", "BIOL", "CAL1", "CAL2", "LAB", "REST",
-        "HASS-A", "HASS-H", "HASS-S", "CI-H", "CI-HW"
-      ]
+      dragSemesterNum: -1
     }
   },
   methods: {
