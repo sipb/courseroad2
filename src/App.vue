87991ef18bac8aa884a7225c516f34016515b4ed
Georgia Shay <georgiashay@gmail.com>
2019-04-02 20:28:51 -0400

    Assign list ids to req tree

--- src/App.vue
+++ src/App.vue
@@ -249,6 +249,7 @@ export default {
     //note for later: will need to use Vue.set on roads for reactivity once they come from fireroad
     roads: {
       "$defaultroad$": {
+        progressOverrides: {},
         downloaded: moment().format(DATE_FORMAT),
         changed: moment().format(DATE_FORMAT),
         name: "My First Road",
