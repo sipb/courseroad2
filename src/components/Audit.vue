6adc04d03a73858c4846a5db4e0ccba18b880bfb
Georgia Shay <georgiashay@gmail.com>
2019-04-14 19:14:21 -0400

    Made logo smaller

--- src/components/Audit.vue
+++ src/components/Audit.vue
@@ -2,7 +2,7 @@
   <!-- useful for adding dropdown: https://vuejs.org/v2/guide/forms.html -->
   <v-flex style="padding: 24px; overflow: auto;">
     <router-link to = "/about" class = "blue-grey lighten-5" style = "color: inherit; text-decoration: none; border-radius: 10px; padding: 8px; display: inline-block;">
-      <v-icon>check_box</v-icon> <h2 style = "display: inline; font-family: Gadget">CourseRoad</h2>
+      <v-icon small >check_box</v-icon> <h3 style = "display: inline; font-family: Gadget">CourseRoad</h3>
     </router-link>
     <v-treeview
       v-model="tree"
