ac91c280a223f645e68f9e154b02ea53edabf5bc
Georgia Shay <georgiashay@gmail.com>
2019-04-14 17:56:13 -0400

    Back arrow

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,5 +1,10 @@
 <template>
   <v-container>
+    <router-link to = "/" style = "color: inherit; text-decoration: none;">
+      <v-btn icon flat>
+        <v-icon>arrow_back</v-icon>
+      </v-btn>
+    </router-link>
     <h1 class = "text-sm-center">About CourseRoad</h1>
   </v-container>
 </template>
