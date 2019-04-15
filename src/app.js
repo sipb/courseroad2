faaf525b054606a2f164223371f89b2759df357e
Georgia Shay <georgiashay@gmail.com>
2019-04-15 00:57:11 -0400

    Renamed BaseApp to App

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import App from './BaseApp.vue'
+import App from './App.vue'
 import MainPage from './pages/MainPage.vue'
 import About from "./pages/About.vue"
 import Vuetify from 'vuetify'
