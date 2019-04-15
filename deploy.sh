452ce3f0af987d921ff9759191a908030eba60a2
Nate Foss <npfoss@gmail.com>
2019-04-15 17:59:29 -0400

    wip

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 npm run build
 # scp -r dist npfoss@athena.dialup.mit.edu:
-ssh npfoss@athena.dialup.mit.edu
-aklog athena sipb
+ssh npfoss@athena.dialup.mit.edu 'aklog athena sipb && echo "test"'
 # mv dist/* /mit/courseroad/web_scripts/courseroad/dev/`
