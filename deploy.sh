b0f02e7021d5af4f59b44b3d57e818095036981e
Georgia Shay <georgiashay@gmail.com>
2019-04-15 19:34:23 -0400

    Initial script setup

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
-npm run build
-# scp -r dist npfoss@athena.dialup.mit.edu:
-ssh npfoss@athena.dialup.mit.edu 'aklog athena sipb && echo "test"'
-# mv dist/* /mit/courseroad/web_scripts/courseroad/dev/`
+npm run build-$1
+scp -r dist $2@athena.dialup.mit.edu: $3
+if ["$1" -eq "dev"]; then
+  ssh $2@athena.dialup.mit.edu $3 'aklog athena sipb && mv dist/* /mit/courseroad/web_scripts/courseroad/dev/'
+else
+  ssh $2@athena.dialup.mit.edu $3 'aklog athena sipb && mv dist/* /mit/courseroad/web_scripts/courseroad/'
+fi
