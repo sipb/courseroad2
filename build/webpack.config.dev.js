78dffb11a9b3148af2ce3f0d1231760617498b19
Michael William Le Nguyen <michael@mail.ttp.codes>
2019-04-15 17:30:49 -0400

    Set callback URL based on env

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -2,46 +2,48 @@
 const webpack = require('webpack')
 const { VueLoaderPlugin } = require('vue-loader')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-module.exports = {
-  mode: 'development',
-
-  entry: [
-    './src/app.js'
-  ],
-  devServer: {
-    hot: true,
-    watchOptions: {
-      poll: true
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        use: [
-          'vue-loader'
-        ]
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'vue-style-loader',
-          'css-loader'
-        ],
-      },
-      {
-        test: /\.js$/,
-        use: 'babel-loader'
+module.exports = (env) => {
+  return {
+    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+    entry: [
+      './src/app.js'
+    ],
+    devServer: {
+      hot: true,
+      watchOptions: {
+        poll: true
       }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          use: [
+            'vue-loader'
+          ]
+        },
+        {
+          test: /\.css$/,
+          use: [
+            'vue-style-loader',
+            'css-loader'
+          ],
+        },
+        {
+          test: /\.js$/,
+          use: 'babel-loader'
+        }
+      ]
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new VueLoaderPlugin(),
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: 'index.html',
+        inject: true
+      }),
+      new webpack.DefinePlugin({ 'process.env.APP_URL': JSON.stringify(env.APP_URL) })
     ]
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new VueLoaderPlugin(),
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'index.html',
-      inject: true
-    })
-  ]
+  }
 }
