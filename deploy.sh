31e909d883977f908e4e66abad44436f60177796
Georgia Shay <georgiashay@gmail.com>
2019-04-15 20:41:44 -0400

    Made script safer and commented out prod

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 #syntax: ./deploy.sh [dev or prod] [kerberos]
 npm run build-$1
 scp -r -o GSSAPIAuthentication=yes -o GSSAPIDelegateCredentials=yes dist $2@athena.dialup.mit.edu:
-if [ "$1" = "dev" ]; then
+if [ "$1" = "prod" ]; then
+  echo "You shouldn't be pushing to prod right now"
+  #ssh $2@athena.dialup.mit.edu -K 'aklog athena sipb && mv dist/* /mit/courseroad/web_scripts/courseroad/'
+elif [ "$1" = "dev" ]; then
   ssh $2@athena.dialup.mit.edu -K 'aklog athena sipb && mv dist/* /mit/courseroad/web_scripts/courseroad/dev/'
 else
-  ssh $2@athena.dialup.mit.edu -K 'aklog athena sipb && mv dist/* /mit/courseroad/web_scripts/courseroad/'
+  echo "Invalid build location"
 fi
