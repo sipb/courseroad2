85996ea47eae55bc929305627a593192bf54864c
Michael William Le Nguyen <michael@mail.ttp.codes>
2019-04-23 23:51:14 -0400

    Bundle styling locally

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -1,6 +1,7 @@
 'use strict'
 const webpack = require('webpack')
 const { VueLoaderPlugin } = require('vue-loader')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = (env) => {
   return {
@@ -17,6 +18,20 @@ module.exports = (env) => {
     module: {
       rules: [
         {
+          // This rule in webpack checks for font files and copies them to the output directory:
+          // - The first section checks for files of type woff, woff2, ttf, eot, svg.
+          // - The second part checks for a possible query string indicating version, such as ?v=1.2.3.
+          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+          use: [{
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: './fonts',
+              publicPath: '../fonts'
+            }
+          }]
+        },
+        {
           test: /\.vue$/,
           use: [
             'vue-loader'
@@ -25,9 +40,11 @@ module.exports = (env) => {
         {
           test: /\.css$/,
           use: [
-            'vue-style-loader',
+            {
+              loader: MiniCssExtractPlugin.loader
+            },
             'css-loader'
-          ],
+          ]
         },
         {
           test: /\.js$/,
@@ -42,6 +59,9 @@ module.exports = (env) => {
         filename: 'index.html',
         template: 'index.html',
         inject: true
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'css/app.css'
       }),
       new webpack.DefinePlugin({ 'process.env.APP_URL': JSON.stringify(env.APP_URL) }),
       new webpack.DefinePlugin({ 'process.env.FIREROAD_URL': JSON.stringify(env.FIREROAD_URL) })
