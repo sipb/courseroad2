dfc3a130dedc3868dcc45bdfbc69da8ec709a082
Georgia Shay <33201801+georgiashay@users.noreply.github.com>
2019-08-22 23:40:02 -0400

    Don't show schedule warning if subject not offered in upcoming semester (#289)

--- src/mixins/schedule.js
+++ src/mixins/schedule.js
@@ -7,8 +7,8 @@ export default {
         const fallCutoff = new Date(year, 4, 15);
         const springCutoff = month === 11 ? new Date(year, 11, 15) : new Date(year - 1, 11, 15);
         const scheduleForFall = month >= 4 && month <= 10;
-        const lateForFall = scheduleForFall && compareDate > fallCutoff;
-        const lateForSpring = !scheduleForFall && compareDate > springCutoff;
+        const lateForFall = scheduleForFall && subj.offered_fall && compareDate > fallCutoff;
+        const lateForSpring = !scheduleForFall && subj.offered_spring && compareDate > springCutoff;
         return lateForFall || lateForSpring;
       }
       return false;
