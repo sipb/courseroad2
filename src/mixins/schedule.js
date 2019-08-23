a5ccfb8ad948e8c88ca6f7b2d15fc13a48b68388
Georgia Shay <georgiashay@gmail.com>
2019-08-22 22:49:21 -0400

    Only show warning if subject is offered in upcoming semester

--- src/mixins/schedule.js
+++ src/mixins/schedule.js
@@ -1,14 +1,15 @@
 export default {
   methods: {
     lateSchedule: function(subj, genericIndex, compareDate = new Date()) {
+      console.log(subj);
       if (subj.schedule === undefined && !(subj.subject_id in genericIndex)) {
         const year = compareDate.getFullYear();
         const month = compareDate.getMonth();
         const fallCutoff = new Date(year, 4, 15);
         const springCutoff = month === 11 ? new Date(year, 11, 15) : new Date(year-1, 11, 15);
         const scheduleForFall = month >= 4 && month <= 10;
-        const lateForFall = scheduleForFall && compareDate > fallCutoff;
-        const lateForSpring = !scheduleForFall && compareDate > springCutoff;
+        const lateForFall = scheduleForFall && subj.offered_fall && compareDate > fallCutoff;
+        const lateForSpring = !scheduleForFall && subj.offered_spring && compareDate > springCutoff;
         return lateForFall || lateForSpring;
       }
       return false;
