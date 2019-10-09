e2422d98ea2bc660444c02421b4fd61d42207c3f
Georgia Shay <georgiashay@gmail.com>
2019-10-09 00:14:31 -0400

    Use every instead of reduce

--- src/components/ClassSearch.vue
+++ src/components/ClassSearch.vue
@@ -292,10 +292,9 @@ export default {
 
       // Filter subjects that match all filter sets and the text filter
       const filteredSubjects = this.allSubjects.filter(function (subject) {
-        var matches = true;
-        for (var filterGroup in this.allFilters) {
-          matches = matches && this.allFilters[filterGroup].matches(subject, this.chosenFilters[filterGroup], { nameInput: this.nameInput });
-        }
+        var matches = Object.keys(this.allFilters).every((filterGroup) => {
+          return this.allFilters[filterGroup].matches(subject, this.chosenFilters[filterGroup], { nameInput: this.nameInput });
+        })
         matches = matches & textFilter.matches(subject, { nameInput: this.nameInput });
         return matches;
       }.bind(this));
