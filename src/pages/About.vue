33c7715b5b60d1d65c1eab30cd35730a5b2d2e6e
Nate Foss <npfoss@gmail.com>
2019-12-08 22:40:54 -0500

    grid update 1

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -7,23 +7,23 @@
     </router-link>
     <div class="text-sm" style="margin: 0px auto; max-width: 48em;">
       <h1>About CourseRoad</h1>
-      <p class="px-3">
+      <p class="px-4">
         CourseRoad is a student-run academic planner for MIT students.
         It allows students to plan out their degrees, test out paths for different
         majors, and view class information. CourseRoad is integrated with FireRoad,
         so you can download the FireRoad app to plan your degree on the go!
       </p>
-      <p class="px-3">
+      <p class="px-4">
         This is always a work in progress! If you have feature requests or
         want to contribute, the source code is open source and located on
         <a target="_blank" href="https://github.com/npfoss/courseroad2">Github</a>.
       </p>
-      <p class="px-3">
+      <p class="px-4">
         We would like to thank the Office of the First Year and the
         Student Information Processing Board for all their support with this project.
       </p>
       <h2>Known Issues</h2>
-      <p class="px-3">
+      <p class="px-4">
         <ul style="display: inline-block;">
           <li>
             <h3>Class year</h3>
@@ -91,22 +91,22 @@
         </ul>
       </p>
       <h2>Old CourseRoad</h2>
-      <p class="px-3">
+      <p class="px-4">
         The old CourseRoad has been moved to <a target="_blank" href="https://courseroad.mit.edu/old">courseroad.mit.edu/old</a>.
         You can export your roads from the old CourseRoad and import them here to transfer your data.
       </p>
       <h2>FireRoad</h2>
-      <p class="px-3">
+      <p class="px-4">
         We would like to thank FireRoad for collaborating with us
         on this project and allowing us to integrate the two services. FireRoad
         is available on for Android and iOS. To read more about FireRoad or request
         an update to course requirements, visit the <a target="_blank" href="https://fireroad.mit.edu">
           FireRoad website</a>.
       </p>
-      <h3 class="px-3">
+      <h3 class="px-4">
         Github Repositories
       </h3>
-      <p class="px-3">
+      <p class="px-4">
         <ul style="display: inline-block;">
           <li><a target="_blank" href="https://github.com/venkatesh-sivaraman/FireRoad">iOS App</a></li>
           <li><a target="_blank" href="https://github.com/venkatesh-sivaraman/FireRoad-Android">Android App</a></li>
@@ -114,13 +114,13 @@
         </ul>
       </p>
       <a name="fireroad" />
-      <p class="px-3">
+      <p class="px-4">
         Download the app for <a target="_blank" href="https://itunes.apple.com/us/app/fireroad-mit-course-planner/id1330678450">iOS</a>
         or <a target="_blank" href="https://play.google.com/store/apps/details?id=com.base12innovations.android.fireroad&hl=en_US">Android</a>.
       </p>
 
       <h2>Maintainers</h2>
-      <p class="px-3">
+      <p class="px-4">
         <ul style="display: inline-block;">
           <li>Nate Foss (npfoss)</li>
           <li>Georgia Shay (gshay)</li>
@@ -128,15 +128,15 @@
           <li>Sophie Mori (ksmori)</li>
         </ul>
       </p>
-      <h3 class="px-3">
+      <h3 class="px-4">
         Past
       </h3>
-      <p class="px-3">
+      <p class="px-4">
         <ul style="display: inline-block;">
           <li>Danny Ben-David</li>
         </ul>
       </p>
-      <p class="px-3">
+      <p class="px-4">
         If you have questions or comments, please <a target="_blank" href="mailto:courseroad@mit.edu">email</a> us.
       </p>
     </div>
