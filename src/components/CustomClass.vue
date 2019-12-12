9361f5898475de75b4b715a88951b10394494f68
Maximo Machado <maximo@mit.edu>
2019-12-12 16:18:29 -0500

    Added rudimentary color selector and selector for custom activites

--- src/components/CustomClass.vue
+++ src/components/CustomClass.vue
@@ -13,16 +13,24 @@
                 <v-card-title>
                   <h2>Custom Activities</h2>
                 </v-card-title>
+                <v-select class="px-3" label="Edit an Existing Custom Activity"></v-select>
                 <v-form>
                   <div class="px-3">
-                    <v-text-field label="Short Title" counter=8></v-text-field>
-                    <v-text-field label="Full Title"></v-text-field>
+                    <v-text-field label="Short Title" counter=8 required></v-text-field>
+                    <v-text-field label="Full Title" required></v-text-field>
                     <v-card-text class="px-0"><h3>Units/Hours</h3></v-card-text>
                     <div class="d-flex">
                       <v-text-field label="Units" class="mx-3"></v-text-field>
                       <v-text-field label="In-Class Hours" class="mx-3"></v-text-field>
                       <v-text-field label="Out-of-Class Hours" class="mx-3"></v-text-field>
                     </div>
+                    <v-card-text class="px-0"><h3>Color</h3></v-card-text>
+                    <v-btn-toggle v-model="colorChosen" width="100%" group mandatory>
+                      <v-btn color="red"></v-btn>
+                      <v-btn color="blue"></v-btn>
+                      <v-btn color="green"></v-btn>
+                      <v-btn color="purple"></v-btn>
+                    </v-btn-toggle>
                   </div>
                   <v-card-actions>
                     <v-spacer />
@@ -42,6 +50,7 @@ export default {
     data: function() {
         return {
             viewDialog: false,
+            colorChosen: undefined
         }
     }
 }
