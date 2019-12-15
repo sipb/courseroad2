7a6ab66f7151377f8e437847e41b18f0f55c5341
Georgia Shay <georgiashay@gmail.com>
2019-12-15 16:57:52 -0500

    Add filter for HASS-E

--- src/components/ClassSearch.vue
+++ src/components/ClassSearch.vue
@@ -91,7 +91,8 @@ export default {
           { name: 'Any', short: 'Any', filterString: 'HASS' },
           { name: 'Art', short: 'A', filterString: 'HASS-A' },
           { name: 'Social Science', short: 'S', filterString: 'HASS-S' },
-          { name: 'Humanity', short: 'H', filterString: 'HASS-H' }
+          { name: 'Humanity', short: 'H', filterString: 'HASS-H' },
+          { name: 'Elective', short: 'E', filterString: 'HASS-E'}
         ],
         ciInput: [
           { name: 'Any', short: 'Any', filterString: 'CI.+' },
