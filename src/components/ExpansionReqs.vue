9f0dbbf5fe0a1e18eb31e41778ba22484a8533ac
Georgia Shay <33201801+georgiashay@users.noreply.github.com>
2020-02-08 23:14:32 -0500

    Prereq indicators (#360)
    * Lighten prereqs that aren't fulfilled
    
    * Retain expansion req state when classes are moved around
    
    * Make nextReqs computed so changes propagate
    
    * lint
    
    * Lighten a bit more
    
    * Do not modify original subjects info objects with fulfilled key
    
    * _this -> this
    
    * Green checkmarks for completed prereqs
    
    * line formatting
    
    * remove duplicate method
    
    * lint
    
    * Rename nextReqs to getNextReqs
    
    Co-authored-by: Nate Foss <npfoss@gmail.com>

--- src/components/ExpansionReqs.vue
+++ src/components/ExpansionReqs.vue
@@ -33,10 +33,10 @@
       </div>
     </div>
     <subject-scroll v-else :subjects="requirement.reqs" @click-subject="clickSubject" />
-    <div v-if="open && nextReqs !== undefined" class="expanded-req">
+    <div v-if="open && getNextReqs !== undefined" class="expanded-req">
       <ExpansionReqs
-        :requirement="nextReqs"
-        :req-i-d="reqID + (doubleScroller ? '.' + whichScroll : '') + '.' + expansionIndex"
+        :requirement="getNextReqs"
+        :req-i-d="reqID + (doubleScroller ? '.' + whichScroller : '') + '.' + expansionIndex"
         @close-expansion="closeMyExpansion"
         @click-subject="$emit('click-subject',$event)"
       />
@@ -68,8 +68,9 @@ export default {
     return {
       open: false,
       expansionIndex: 0,
-      nextReqs: undefined,
-      whichScroller: 0
+      whichScroller: 0,
+      scrollerClicked: undefined,
+      subjectClicked: undefined
     };
   },
   computed: {
@@ -80,15 +81,29 @@ export default {
         }, true);
       }
       return false;
+    },
+    getNextReqs: function () {
+      if (!this.open) {
+        return undefined;
+      }
+
+      if (this.scrollerClicked !== undefined) {
+        return this.requirement.reqs[this.scrollerClicked].reqs[this.subjectClicked.index];
+      } else {
+        return this.requirement.reqs[this.subjectClicked.index];
+      }
     }
   },
   watch: {
-    requirement: function (newReq, oldReq) {
+    reqID: function (newReq, oldReq) {
       this.open = false;
     }
   },
   methods: {
     clickSubject: function (subj, scroller) {
+      this.scrollerClicked = scroller;
+      this.subjectClicked = subj;
+
       let scrollPointID;
       let nextReqs;
       if (scroller !== undefined) {
@@ -98,10 +113,10 @@ export default {
         scrollPointID = this.reqID + '.' + subj.index;
         nextReqs = this.requirement.reqs[subj.index];
       }
+
       if (nextReqs.reqs !== undefined) {
         this.expansionIndex = subj.index;
         this.open = true;
-        this.nextReqs = nextReqs;
         Vue.nextTick(function () {
           const scrollPoint = $('#' + $.escapeSelector(scrollPointID));
           const topPoint = scrollPoint.offset().top;
