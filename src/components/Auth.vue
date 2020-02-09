0f12bb1a1d1a76f75a0d775adde5122f1672562c
Georgia Shay <georgiashay@gmail.com>
2020-02-08 22:00:41 -0500

    Renumber local roads before attempting to send to server

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -277,9 +277,6 @@ export default {
     },
     getUserData: function () {
       this.gettingUserData = true;
-      for (var i = 0; i < this.newRoads.length; i++) {
-        this.saveRemote(this.newRoads[i]);
-      }
       this.getSecure('/sync/roads/')
         .then(function (response) {
           if (response.status === 200 && response.data.success) {
@@ -289,6 +286,9 @@ export default {
           }
         }).then(function (files) {
           this.renumberRoads(files);
+          for (var i = 0; i < this.newRoads.length; i++) {
+            this.saveRemote(this.newRoads[i]);
+          }
           const fileKeys = Object.keys(files);
           for (var i = 0; i < fileKeys.length; i++) {
             const blankRoad = {
