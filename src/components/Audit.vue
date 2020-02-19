ddecee8b90a33b600d5a3865aa64728946225692
HeadMerchant <amanitoussaintgreen@gmail.com>
2020-02-18 19:46:16 -0500

    Added link to Course 6 Student Portal (#379)
    * Added link to Course 6 Student Portal
    
    Computed property 'isCourse6' checks if any of the user's courses are in 6.
    
    Link is rendered below the requirements tree and only if the user is a Course 6
    
    * lint
    
    * Make correct courses link to 6 audit
    
    * Check if courses have loaded yet
    
    * move into main function body
    
    * tweak wording
    
    * Increased space between link and
    
    * Lint fix
    
    Co-authored-by: Georgia Shay <33201801+georgiashay@users.noreply.github.com>
    Co-authored-by: miriam-rittenberg <48919424+miriam-rittenberg@users.noreply.github.com>
    Co-authored-by: Nate Foss <npfoss@gmail.com>

--- src/components/Audit.vue
+++ src/components/Audit.vue
@@ -49,6 +49,13 @@
       </template>
     </v-treeview>
 
+    <p v-if="isCourse6">
+      <br>
+      <a href="http://eecsappsrv.mit.edu/students/">
+        Course 6 Student Portal (+Audit)
+      </a>
+    </p>
+
     <v-dialog v-model="progressDialog" max-width="600">
       <v-card v-if="progressReq !== undefined">
         <v-btn icon flat style="float:right" @click="progressDialog=false">
@@ -186,6 +193,18 @@ export default {
           this.$store.commit('addReq', newReq);
         }
       }
+    },
+    isCourse6: function () {
+      return this.selectedTrees.some(course => {
+        // checks if a course is one of the ones that the course 6 audit page works for
+        // if statement needed because otherwise it gives an error if the courses haven't loaded yet
+        if (course['short-title']) {
+          const major = course['short-title'].slice(0, 3);
+          return ['6-1', '6-2', '6-3', '6-7', '6-14', '6', '6-P'].includes(major);
+        } else {
+          return false;
+        }
+      });
     },
     getCourses: function () {
       const courses = this.reqList.slice(0);
