ede7801a02b4b5f66af0bbc06e79e4e74c1a2a51
Nate Foss <npfoss@gmail.com>
2020-02-29 16:57:27 -0500

    fix prod dir typo

--- deploy.sh
+++ deploy.sh
@@ -25,10 +25,10 @@ if [ "$1" = "prod" ]; then
 	kinit -f -l 1h $2
 	aklog sipb
       fi
-      rsync --delete --progress --checksum -r deploy/development/.htaccess dist/* /afs/sipb.mit.edu/project/courseroad/web_scripts/courseroad/
+      rsync --delete --progress --checksum -r deploy/production/.htaccess dist/* /afs/sipb.mit.edu/project/courseroad/web_scripts/courseroad/
     else
       echo "Could not locate AFS, using SSH for deployment"
-      scp -r deploy/development/.htaccess dist/* $2@athena.dialup.mit.edu:/mit/courseroad/web_scripts/courseroad/
+      scp -r deploy/production/.htaccess dist/* $2@athena.dialup.mit.edu:/mit/courseroad/web_scripts/courseroad/
     fi
   else
     echo "Cancelled"
