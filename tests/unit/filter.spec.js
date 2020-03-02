2d74527fde04c5379faba8a5373b761db3afa5c4
Georgia Shay <georgiashay@gmail.com>
2020-03-02 00:00:59 -0500

    Placeholder test

--- tests/unit/filter.spec.js
+++ tests/unit/filter.spec.js
@@ -1,1 +1,9 @@
 import { FilterGroup, Filter, RegexFilter, MathFilter, BooleanFilter } from '../../src/utilities/filters.js';
+
+describe('Filters', () => {
+  it('does one thing right', () => {
+    let f = new Filter("test", "t", ((x) => x === 7), ['y'], 'AND');
+    expect(f.matches({y: 7})).toBeTruthy();
+    expect(f.matches({y: 8})).toBeFalsy();
+  })
+});
