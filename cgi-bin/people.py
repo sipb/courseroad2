06ae65fbf5fa149972680d1f0aaef831579847ad
Michael William Le Nguyen <michael@mail.ttp.codes>
2020-04-03 13:21:10 -0700

    Condense print statements in CGI script

--- cgi-bin/people.py
+++ cgi-bin/people.py
@@ -8,12 +8,11 @@ from requests import get
 args = FieldStorage()
 
 error = 'Status: 400 Bad Request\n'
-content = 'Content-Type: application/json\n\n'
+header = 'Content-Type: application/json\n\n'
 
 if 'kerb' not in args:
     output = {"error": "kerb was not specified"}
-    print(error + content)
-    print(dumps(output))
+    header = error + header
 else:
     c = ConfigParser()
     try:
@@ -21,28 +20,25 @@ else:
             c.read_file(fp)
         response = get('https://mit-people-v3.cloudhub.io/people/v3/people/{0}'.format(args['kerb'].value), headers={'client_id': c['Credentials']['ID'], 'client_secret': c['Credentials']['Secret']})
         if response.status_code != 200:
+            header = error + header
             output = {"error": "could not get user data"}
-            print(error + content)
-            print(dumps(output))
         else:
             data = loads(response.text)
             if data['item']['affiliations'][0]['type'] != "student":
+                header = error + header
                 output = {"error": "user is not a student"}
-                print(error + content)
-                print(dumps(output))
             else:
                 year = data['item']['affiliations'][0]['classYear']
                 if year == "G":
+                    header = error + header
                     output = {"error": "user is a graduate student (currently unhandled)"}
-                    print(error + content)
-                    print(dumps(output))
                 else:
                     year = int(year)
                     year = year - 1
                     output = {"year": year}
-                    print(content)
-                    print(dumps(output))
     except Exception:
+        header = error + header
         output = {"error": "could not read credentials"}
-        print(error + content)
-        print(dumps(output))
+
+print(header)
+print(dumps(output))
