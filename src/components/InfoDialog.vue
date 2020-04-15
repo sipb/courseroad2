8205b854f97c25170915d905176f9e48e4977545
Maximo Machado <maximo@mit.edu>
2020-04-15 00:53:00 -0400

    Remove old petition from InfoDialog

--- src/components/InfoDialog.vue
+++ src/components/InfoDialog.vue
@@ -1,4 +1,3 @@
-<!-- TODO Delete Deprecated Petition Code here -->
 <template>
   <v-dialog v-model="viewDialogChild" max-width="600">
     <div v-if="dialogReq !== undefined">
@@ -24,22 +23,6 @@
         <v-card-text v-if="'req' in dialogReq">
           {{ dialogReq["req"] }}
         </v-card-text>
-        <div style="display:flex">
-          <v-card-text style="width:auto" class="center-text">
-            <b>Petition this Requirement:</b>
-          </v-card-text>
-          <v-autocomplete
-            v-model="activeProgressAssertions"
-            menu-props="lazy"
-            :items="removeSharedCourses(selectedSubjects.flat(), dialogReq['sat_courses']).sort(sortCourses)"
-            item-text="id"
-            label="Select Class"
-            no-data-text="No Courses Found"
-            chips
-            deletable-chips
-            multiple
-          />
-        </div>
         <v-card-text>
           <b>Satisfying courses:</b>
           <div v-for="course in dialogReq['sat_courses']" :key="course">
@@ -75,28 +58,6 @@ export default {
       viewDialogChild: false
     };
   },
-  computed: {
-    activeProgressAssertions: {
-      get: function () {
-        const parsedDialogReq = this.dialogReq.uniqueKey.split('-')[1]; // Removes index from req key ex. "0-girs" to "girs"
-        const dialogReqPA = this.$store.state.roads[this.$store.state.activeRoad].contents.progressAssertions[parsedDialogReq];
-        // Checks if unique key in progressAssert, if it is, searches for substitution key
-        if (dialogReqPA === undefined) {
-          return dialogReqPA;
-        } else {
-          return dialogReqPA['substitutions'];
-        }
-      },
-      set: function (newReqs) {
-        this.$store.commit('setProgressAssertions', { uniqueKey: this.dialogReq.uniqueKey, newReqs: newReqs });
-      }
-    },
-    progressAssertions: {
-      get: function () {
-        return this.$store.state.roads[this.$store.state.activeRoad].contents.progressAssertions;
-      }
-    }
-  },
   watch: {
     viewDialogChild (newVal) {
       this.$emit('update:view-dialog', newVal);
@@ -121,38 +82,6 @@ export default {
     deleteReq: function (req) {
       const reqName = req['list-id'].substring(0, req['list-id'].indexOf('.reql'));
       this.$store.commit('removeReq', reqName);
-    },
-    removeSharedCourses: function (courses, coursesToFilter) {
-      // Takes two arrays of courses and returns an array without the courses in coursesToFilter
-      return courses.filter(function (el) {
-        for (let i = 0; i < coursesToFilter.length; i++) {
-          if (el.id === coursesToFilter[i]) {
-            return false;
-          }
-        }
-        return true;
-      });
-    },
-    sortCourses: function (courseOne, courseTwo) {
-      // Key function to sort courses into major order. With courses with letters within the id it will be ordered based on semester placed in.
-      const courseOneId = courseOne.id;
-      const courseTwoId = courseTwo.id;
-      if (isNaN(parseFloat(courseOneId)) && isNaN(parseFloat(courseTwoId))) {
-        const majorOneId = courseOneId.substring(0, 2);
-        const majorTwoId = courseTwoId.substring(0, 2);
-        if (majorOneId.localeCompare(majorTwoId) === 0) {
-          const courseNumberOne = courseOneId.substring(courseOneId.lastIndexOf('.') + 1);
-          const courseNumberTwo = courseTwoId.substring(courseTwoId.lastIndexOf('.') + 1);
-          return parseInt(courseNumberOne) - parseInt(courseNumberTwo);
-        }
-        return majorOneId.localeCompare(majorTwoId);
-      } else if (isNaN(parseFloat(courseOneId))) {
-        return 1;
-      } else if (isNaN(parseFloat(courseTwoId))) {
-        return -1;
-      } else {
-        return parseFloat(courseOneId) - parseFloat(courseTwoId);
-      }
     }
   }
 };
