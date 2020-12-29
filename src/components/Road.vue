e75b9fe4747bae32b0d6667659ab7dc6f23b5e49
Georgia Shay <33201801+georgiashay@users.noreply.github.com>
2020-12-28 21:26:27 -0500

    Add validation for visible list prop (#429)
    * Add validation for visible list prop
    
    * Remove invalidated cookies

--- src/components/Road.vue
+++ src/components/Road.vue
@@ -117,7 +117,11 @@ export default {
   mounted () {
     const visibleList = JSON.parse(this.$cookies.get('visibleList' + this.roadID));
     if (this.$store.state.cookiesAllowed && visibleList) {
-      this.visibleList = visibleList;
+      if (Array.isArray(visibleList) && visibleList.length == this.numSems) {
+        this.visibleList = visibleList;
+      } else {
+        this.$cookies.remove('visibleList' + this.roadID);
+      }
     };
   }
 };
