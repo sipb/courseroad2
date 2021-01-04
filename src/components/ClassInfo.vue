7737dd7fdcaaebdb4681beca1439c5290e9fcf2b
w-nicole <wnicole1549@gmail.com>
2021-01-03 20:00:59 -0500

    revised_variable_use

--- src/components/ClassInfo.vue
+++ src/components/ClassInfo.vue
@@ -270,9 +270,8 @@ export default {
       if (thisGIRAttr === undefined) {
         IDMatcher = new RegExp('(^|[^\\da-zA-Z])' + currentID.replace(/[.*+?^${}()|[\]\\]/g, '\\$&') + '(?![\\da-zA-Z])');
       } else { // Expression taken directly from above, but modified for GIRs
-        thisGIRAttr = 'GIR:' + thisGIRAttr;
         var filteredCurrentID = currentID.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-        IDMatcher = new RegExp('(^|[^\\da-zA-Z])' + '(' + thisGIRAttr + '|' + filteredCurrentID + ')' + '(?![\\da-zA-Z])');
+        IDMatcher = new RegExp('(^|[^\\da-zA-Z])' + '(' + 'GIR:' + thisGIRAttr + '|' + filteredCurrentID + ')' + '(?![\\da-zA-Z])');
       }
       return this.$store.state.subjectsInfo.filter(function (s) {
         return s.prerequisites !== undefined && IDMatcher.test(s.prerequisites);
