6b86be6b8bdb8c49759dd53630f8f617d6135baf
Darren <47044879+tazadejava@users.noreply.github.com>
2021-10-24 13:34:23 -0400

    Add stack logic check (#467)
    * Add stack logic check
    
    * Fix format
    
    * Fix at test compatibility issue

--- src/stores/courseData.js
+++ src/stores/courseData.js
@@ -230,7 +230,9 @@ const store = new Vuex.Store({
     },
     pushClassStack (state, id) {
       if (id in state.subjectsIndex || id in state.genericIndex) {
-        state.classInfoStack.push(id);
+        if (state.classInfoStack.length === 0 || id !== state.classInfoStack[state.classInfoStack.length - 1]) { // we don't want to push to the stack if we click the same class over and over again
+          state.classInfoStack.push(id);
+        }
       }
     },
     removeClass (state, { classInfo, classIndex }) {
