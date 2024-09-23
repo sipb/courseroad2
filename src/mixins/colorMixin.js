e9380b9a24c6f7cac4e4327edeb0bd38dedf1100
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2024-09-23 00:46:05 -0400

    slightly lighter text colors

--- src/mixins/colorMixin.js
+++ src/mixins/colorMixin.js
@@ -171,7 +171,9 @@ export default {
       const g = parseInt(this.colors[courseColor].substring(3, 5), 16);
       const b = parseInt(this.colors[courseColor].substring(5, 7), 16);
 
-      return r * 0.299 + g * 0.587 + b * 0.114 > 186 ? "#000000" : "#ffffff";
+      return r * 0.299 + g * 0.587 + b * 0.114 > 186
+        ? "#000000bb"
+        : "#ffffffee";
     },
     // courseColor takes in subject
     courseColor: function (subject) {
